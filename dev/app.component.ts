b05e1f0e530d43bb673daccd72d2a56d10b5eaae
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-15 10:26:48 +0300

    removed onTest function added  parent section

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,16 +1,21 @@
 import {Component} from 'angular2/core';
+import {PropertyComponent} from "./property-binding.component";
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>{{ onTest()}}</p>
-        <input type="text" value="{{name}}">
+    <section class="parent">
+        <h2>This is the parent component</h2>
+        <h4>Please enter your name:</h4>
+        <input type="text" [(ngModel)]="name">
+        <br><br>
+        <section class="child">
+            <my-property-binding [myName]="name" [myAge]="24"></my-property-binding>
+        </section>
+    </section>
     `,
+    directives: [PropertyComponent]
 })
 export class AppComponent {
-    name = 'Lawrence';
-    onTest(){
-        return 1===1;
-    }
+    name = ''
 }
