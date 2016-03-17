627c9076a5e89a5b0058f0a2004f0d870f8e903d
Lawrence Macharia <macharialawrence2011@gmail.com>
2016-03-17 19:53:18 +0300

    included my custom component <my-input></my-input> and also imported it

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,23 +1,19 @@
 import {Component} from 'angular2/core';
-import {PropertyComponent} from "./property-binding.component";
+import {InputComponent} from "./bindings/input.component";
 
 @Component({
     selector: 'my-app',
     template: `
-    <section class="parent">
-        <h2>This is the parent component</h2>
-        <h4>Please enter your name:</h4>
-        <input type="text" [(ngModel)]="name">
-        <br><br>
-        <section class="child">
-            <my-property-binding [myName]="name" [myAge]="24" (hobbiesChanged)="hobbies = $event"></my-property-binding>
-        </section>
-        <p>My hobbies are: {{hobbies}}</p>
-    </section>
+        <div class= "container">
+            <my-input></my-input>
+        </div>
+        <div class= "container">
+            ...
+        </div>
     `,
-    directives: [PropertyComponent]
+    directives: [InputComponent]
 })
 export class AppComponent {
-    name = '';
-    hobbies = '';
+    myself = {name: '', age:''};
+    confirmedMyself = {name: '', age:''};
 }
